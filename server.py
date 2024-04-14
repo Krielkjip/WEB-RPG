c01fd7344b13d4acae1179820ab9ea444239de5d
Krielkjip <onnotevoortwis@gmail.com>
2024-04-14 20:57:29 +0200

    check if map is something, small changes

--- server.py
+++ server.py
@@ -15,7 +15,8 @@ from fastapi.responses import StreamingResponse
 from starlette.responses import HTMLResponse
 
 
-map = [["1","2","3"],["4","5","6"],["7","8","9"]]
+map = [["1","2","3","4"],["5","6","7","8"],["9","10","11","12"],["13","14","15","16"]]
+# map = None
 
 
 # Get the directory of the current Python script
