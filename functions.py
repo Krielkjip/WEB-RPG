00f71dcb5ed185f1473cd179e5318340cdcd3684
Krielkjip <onnotevoortwis@gmail.com>
2024-05-14 14:32:24 +0200

    fixed change region map bug

--- functions.py
+++ functions.py
@@ -244,10 +244,10 @@ def move_player(move, map_size, region_map_size, player_state, where):
 def collect_resource(interact, player_state, region_map):
     if interact == "Cut Tree":
         player_state["inventory"]["logs_amount"] += 1
-        region_map[player_state["region_location"][0]][player_state["region_location"][1]] = "Grass"
+        region_map[player_state["region_location"][1]][player_state["region_location"][0]] = "Grass"
     elif interact == "Mine Rock":
         player_state["inventory"]["rocks_amount"] += 1
-        region_map[player_state["region_location"][0]][player_state["region_location"][1]] = "Dirt"
+        region_map[player_state["region_location"][1]][player_state["region_location"][0]] = "Dirt"
     return player_state, region_map
 
 
